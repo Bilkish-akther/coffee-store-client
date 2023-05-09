f80249b3b718b580580f840cea5d064fc6aa6395
bilkish-akther <bilkishakther921@gmaile.com>
2023-05-09 07:56:27 -0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import AddCoffee from './components/addCoffee.jsx';
+import UpdateCoffee from './components/updateCoffee.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    loader: () => fetch('http://localhost:5000/coffee')
+  },
+  {
+    path: 'addCoffee',
+    element: <AddCoffee></AddCoffee>
+  },
+
+  {
+    path: 'updateCoffee/:id',
+    element: <UpdateCoffee></UpdateCoffee>,
+    loader: ({params}) => fetch(`http://localhost:5000/coffee/${params.id}`)
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <RouterProvider router={router} />
+  </React.StrictMode>,
+)
